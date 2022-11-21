4536d677d7e84e5ddb172e0428ea48200fec6ae8
Carlangasv <carloszzzxc@hotmail.com>
2022-11-20 22:15:28 -0500

    User constraints created

--- src/main/java/perficient/academic/universityapplication/models/User.java
+++ src/main/java/perficient/academic/universityapplication/models/User.java
@@ -1,10 +1,13 @@
 package perficient.academic.universityapplication.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,11 +20,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
-@Table(name = "my_user")
+@Table(name = "my_user", uniqueConstraints = @UniqueConstraint(columnNames = { "governmentId", "email" }))
 public class User
 {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@NotNull
+	private Long governmentId;
 
 	private String name;
 
