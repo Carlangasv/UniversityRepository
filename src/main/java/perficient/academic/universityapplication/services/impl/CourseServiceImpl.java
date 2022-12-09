2a7279d6d1a0b6abdc0daaf1f265606944469e30
Carlangasv <carloszzzxc@hotmail.com>
2022-12-08 23:30:36 -0500

    Refactor to fit in SOLID principles. ControllerAdvisor created to handle all exceptions. NoSuchElementException handled.

--- src/main/java/perficient/academic/universityapplication/services/impl/CourseServiceImpl.java
+++ src/main/java/perficient/academic/universityapplication/services/impl/CourseServiceImpl.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import perficient.academic.universityapplication.models.Course;
+import perficient.academic.universityapplication.models.Subject;
 import perficient.academic.universityapplication.repositories.CourseRepository;
+import perficient.academic.universityapplication.repositories.SubjectRepository;
 import perficient.academic.universityapplication.services.CourseService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -19,6 +22,7 @@ public class CourseServiceImpl implements CourseService
 {
 	private final ModelMapper modelMapper;
 	private final CourseRepository courseRepository;
+	private final SubjectRepository subjectRepository;
 
 	@Override
 	public List<Course> getCourses()
@@ -29,7 +33,8 @@ public class CourseServiceImpl implements CourseService
 	@Override
 	public Course getCourseById(Long courseId)
 	{
-		return getCourseRepository().findById(courseId).orElseThrow(() -> new NoSuchElementException("No such course with id " + courseId));
+		return getCourseRepository().findById(courseId)
+				.orElseThrow(() -> new NoSuchElementException("No such course with id " + courseId));
 	}
 
 	@Override
@@ -41,6 +46,20 @@ public class CourseServiceImpl implements CourseService
 	@Override
 	public Course saveCourse(Course newCourse)
 	{
+		List<Subject> subjects = new ArrayList<>();
+		newCourse.getSubjectList().forEach(e -> {
+			if (subjectRepository.findById(e.getId()).isPresent())
+			{
+				subjects.add(subjectRepository.findById(e.getId()).get());
+			}
+			else
+			{
+				subjects.add(subjectRepository.save(e));
+			}
+		});
+		getCourseRepository().save(newCourse);
+		subjects.forEach(e -> e.setCourse(newCourse));
+		newCourse.setSubjectList(subjects);
 		return getCourseRepository().save(newCourse);
 	}
 
