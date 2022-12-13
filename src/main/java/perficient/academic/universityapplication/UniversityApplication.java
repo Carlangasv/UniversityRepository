2c198ee877ad499cf7efaf85bd2c14e785fe4989
Carlangasv <carloszzzxc@hotmail.com>
2022-12-12 19:37:07 -0500

    Test of new pom.

--- src/main/java/perficient/academic/universityapplication/UniversityApplication.java
+++ src/main/java/perficient/academic/universityapplication/UniversityApplication.java
@@ -2,9 +2,11 @@ package perficient.academic.universityapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 
 @SpringBootApplication
+@Profile(value="dev")
 public class UniversityApplication
 {
 	public static void main(String[] args)
